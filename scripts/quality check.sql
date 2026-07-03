1889885b7adf26715f116c859734c45090a8a01a
C-ivanPS <c.ivanps27@gmail.com>
2026-07-02 22:16:02 -0600

    Update quality check.sql

--- scripts/quality check.sql
+++ scripts/quality check.sql
@@ -31,3 +31,45 @@ WHERE prd_cost < 0 OR prd_cost IS NULL
 SELECT * 
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt
+
+-- Check for Invalid Dates
+
+SELECT 
+NULLIF (sls_order_dt,0)sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+--Check for Invalid Date Orders
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+/*
+Check Dta Consistency: Between Sales, Quantity, and Price
+>>> Sales = Quantity * Price
+>>> Value must not be NULL, Zero, or Negative.
+*/
+
+SELECT DISTINCT
+sls_quantity,
+
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+	THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+
+CASE WHEN sls_price IS NULL OR sls_price <= 0
+	THEN sls_sales / NULLIF(sls_quantity, 0)
+	ELSE sls_price
+END AS sls_price
+
+
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+		OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+		OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
